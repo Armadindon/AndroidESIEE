66b5f2cba9412550d923215b93a73ad5c84d20e6
pellanm <mateo.pellan@gmail.com>
2021-11-29 13:14:16 +0100

    correction vues + changement activité principale + liaisons connection/signup

--- app/src/main/java/com/esiee/openclassroom/SignUp.java
+++ app/src/main/java/com/esiee/openclassroom/SignUp.java
@@ -2,13 +2,40 @@ package com.esiee.openclassroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+
+    private EditText mLoginEditText;
+    private EditText mFirstnameEditText;
+    private EditText mLastnameEditText;
+    private EditText mPasswordEditText;
+    private Button mSignUpButton;
+    private TextView mSignUpLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        mLoginEditText = findViewById(R.id.signup_edittext_login);
+        mFirstnameEditText = findViewById(R.id.signup_edittext_firstname);
+        mLastnameEditText = findViewById(R.id.signup_edittext_lastname);
+        mPasswordEditText = findViewById(R.id.signup_edittext_password);
+        mSignUpButton = findViewById(R.id.signup_button_signup);
+        mSignUpLink = findViewById(R.id.signup_link_connection);
+
+        mSignUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent connectionIntent = new Intent(v.getContext(), Connection.class);
+                startActivityForResult(connectionIntent, 0);
+            }
+        });
     }
 }
