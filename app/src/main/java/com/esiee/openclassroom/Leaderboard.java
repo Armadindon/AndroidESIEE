b72006a01c6bf5dece0c07483a271871c1fa9e36
lindaluu13 <linda.luu@edu.esiee.fr>
2021-12-06 15:44:10 +0100

    styles updated

--- app/src/main/java/com/esiee/openclassroom/Leaderboard.java
+++ app/src/main/java/com/esiee/openclassroom/Leaderboard.java
@@ -1,8 +1,13 @@
 package com.esiee.openclassroom;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,6 +18,7 @@ public class Leaderboard extends AppCompatActivity {
     private List<TextView> ranks;
     private TextView rank;
     private TextView score;
+    private Button mReturn;
 
 
     @Override
@@ -44,5 +50,25 @@ public class Leaderboard extends AppCompatActivity {
         }
         rank.setText("551");
         score.setText("#1");
+
+        mReturn = findViewById(R.id.leaderboard_button_return);
+
+        mReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(Leaderboard.this)
+                        .setTitle("Retour à l'accueil")
+                        .setMessage("Voulez-vous vraiment retourner à l'accueil?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent menuIntent = new Intent(v.getContext(), Menu.class);
+                                startActivityForResult(menuIntent, 0);
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
     }
 }
